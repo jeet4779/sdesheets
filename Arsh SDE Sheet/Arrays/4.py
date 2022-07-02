b940a009b51cb25e9eb5693fcad6c433bfd5b41d
Sarvjeet Singh <63469455+jeet4779@users.noreply.github.com>
2022-07-02 19:22:10 +0530

    Create 4.py

---
+++ Arsh SDE Sheet/Arrays/4.py
@@ -0,0 +1,34 @@
+'''
+Given an m x n integer matrix matrix, if an element is 0, set its entire row and column to 0's.
+
+You must do it in place.
+'''
+class Solution(object):
+    def setZeroes(self, matrix):
+    
+        is_col = False
+        R = len(matrix)
+        C = len(matrix[0])
+        for i in range(R):
+            if matrix[i][0] == 0:
+                is_col = True
+            for j in range(1, C):
+                
+                if matrix[i][j]  == 0:
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
+
+    
+        for i in range(1, R):
+            for j in range(1, C):
+                if not matrix[i][0] or not matrix[0][j]:
+                    matrix[i][j] = 0
+
+        
+        if matrix[0][0] == 0:
+            for j in range(C):
+                matrix[0][j] = 0
+
+        if is_col:
+            for i in range(R):
+                matrix[i][0] = 0
